a565f087d0fb95176bc0e27b386e01413178b376
Sebastian Bellitto <39646084+SebastianSlash@users.noreply.github.com>
2020-09-19 16:10:36 +0200

    LightSensor values

--- laser_detect.py
+++ laser_detect.py
@@ -1,9 +1,11 @@
-from gpiozero import LED
+from gpiozero import LED, LightSensor
 from time import sleep
 from signal import pause
 
 led = LED(2)
+ldr = LightSensor(4)
 
+print(ldr.value)
 led.blink()
 
 pause()
