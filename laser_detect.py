0d9b7c80e4c503f0b872903b4dc7e3638a429484
Sebastian Bellitto <39646084+SebastianSlash@users.noreply.github.com>
2020-09-20 21:29:52 +0200

    little counter

--- laser_detect.py
+++ laser_detect.py
@@ -5,8 +5,21 @@ from signal import pause
 led = LED(2)
 ldr = LightSensor(4)
 
+interrupt = False
+count = 0
+
 while True:
-    ldr.when_dark = led.on()
-    ldr.when_light = led.off()
+    print("Besucher: ", count)
+    while interrupt is False:
+        if ldr.value < 0.1:
+            led.on()
+            count += 1
+            interrupt = True
+        else:
+            led.off()
+    while interrupt is True:
+        if ldr.value > 0.1:
+            led.off()
+            interrupt = False
 
 pause()
